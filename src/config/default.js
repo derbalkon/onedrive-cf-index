ebd40ebda40329077999e1e8b72f05c89e045ec9
derbalkon <to.wrrrr@gmail.com>
2021-01-31 20:27:27 +0800

    config: your repo looks good but now it's mine

--- src/config/default.js
+++ src/config/default.js
@@ -5,7 +5,7 @@ const config = {
    * to get following params: client_id, client_secret, refresh_token & redirect_uri.
    */
   refresh_token: REFRESH_TOKEN,
-  client_id: '6600e358-9328-4050-af82-0af9cdde796b',
+  client_id: '75fffa28-61fe-4a4f-940e-0cc5f0a03bae',
   client_secret: CLIENT_SECRET,
   redirect_uri: 'https://heymind.github.io/tools/microsoft-graph-api-auth',
 
